cfecadc34c8ac767f9c66fe06f290447d53bcc5d
aman174mathur <aman77mathur@gmail.com>
2025-03-14 22:14:09 +0530

    fjwehfipw

--- question3.py
+++ question3.py
@@ -55,14 +55,14 @@
 #         print(l  , end= " ")
 #     print()
 
-n=7
-for i in range(n):
-    for j in range(i+1):
-        if j==0 or j==i or i==n-1:
-            print("#" ,end = " ")
-        else:
-            print(" " , end=" ")
-    print()
+# n=7
+# for i in range(n):
+#     for j in range(i+1):
+#         if j==0 or j==i or i==n-1:
+#             print("#" ,end = " ")
+#         else:
+#             print(" " , end=" ")
+#     print()
 
 # n = 6  # Number of rows
 
@@ -74,10 +74,68 @@ for i in range(n):
 #             print(" ", end="")
 #     print()  # Move to the next line
 
-#
-##
-# #
-#  #
-# #
-##
-# 
+# n = 6  # Number of rows
+
+# for i in range(0,4):
+#     for j in range(i + 1):
+#         if j == 0 or j == i or i == n - 1:
+#             print("#", end="")
+#         else:
+#             print(" ", end="")
+#         for k in range(n-i):
+#             if j == 0 or j == i or i == n - 1:
+#                 print("#", end="")
+#             else:
+#                 print(" ", end="")
+#         print()  # Move to the next line
+
+# n= 7
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" " , end = " ")
+#     for k in range(
+#     for l in range(n-i):
+#         if i==2 or i==4 or i==6:
+#             print("*" , end = " ")
+#         print()
+
+#  ******
+
+#  *   *
+
+#  * *
+
+#   *
+
+
+# num = int(input("Enter the number of rows: "))
+
+# for i in range(num):
+#     for j in range(i+1):
+#         if j == 0 or j ==i or i==num -1:
+#             print("*" , end = " ")
+#         else:
+#             print(" ", end = " ")
+#     print() 
+# for k in range(num):
+#     for l in range(num-k-1):
+#         if l == 0 or l == 
+#         else:
+#             print(" ", end = " ")
+#     print()
+
+# .program to find the frequency of the substring 
+
+# str = "ababa"
+# # output :{'a': 3, 'b': 2 , "ab":2 , "ba":2 , "aba":1 , "bab":1 , "ababa": 1 , "baba":1 } 
+# freq = {}
+# list1 =[]
+# for i in range(len(str)):
+#     for j in range(i+1,len(str)+1):
+#         list1.append(str[i:j])
+# for i in list1:
+#     if i in freq:
+#         freq[i] +=1
+#     else:
+#         freq[i] = 1
+# print(freq)
