87d942ef3eb75daa526d937583fe7ecb50992397
aman174mathur <aman77mathur@gmail.com>
2025-03-20 12:27:49 +0530

    esfwfwf

--- day1.py
+++ day1.py
@@ -84,12 +84,12 @@
 #         print(chr(65+i), end = " ")
 #     print()
 
-n=5
-for i in range(n):
-    for j in range(n-i):
-        print(" " , end = "")
-    for k in range(i+1):
-        print(chr(65+k) , end = " ")
-    for k in range(i-1,-1,-1):
-        print(chr(65+k) , end = " ")
-    print()
+# n=5
+# for i in range(n):
+#     for j in range(n-i):
+#         print(" " , end = "")
+#     for k in range(i+1):
+#         print(chr(65+k) , end = " ")
+#     for k in range(i-1,-1,-1):
+#         print(chr(65+k) , end = " ")
+#     print()
